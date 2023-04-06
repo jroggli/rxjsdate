223d6ec42d1b386f816f7593f7cb8e1a7a5ab51d
jroggli <j.roggli@inova.ch>
2023-04-06 16:32:51 +0200

    navigate works without input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {
   ChangeDetectorRef,
   Component,
 } from '@angular/core';
-import { BehaviorSubject, Subject, combineLatest } from 'rxjs';
+import { BehaviorSubject, Subject, combineLatest, of } from 'rxjs';
 import { map, scan, share, startWith, tap } from 'rxjs/operators';
 @Component({
   selector: 'app',
@@ -11,41 +11,16 @@ import { map, scan, share, startWith, tap } from 'rxjs/operators';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public date$ = new BehaviorSubject<Date>(new Date(Date.UTC(2000, 0, 1)));
-
   public navigate$ = new Subject<number>();
 
-  public result$ = combineLatest([
-    this.date$,
-    this.navigate$.pipe(
-      startWith(0),
-      scan((acc, val) => acc + val, 0)
-    ),
-  ]).pipe(
-    map(([date, navigate]) => {
+  public date$ = this.navigate$.pipe(
+    startWith(0),
+    scan((date, navigate) => {
       date = new Date(date);
       date.setDate(date.getDate() + navigate);
       return date;
-    }),
-    share()
-  );
-
-  public resultText = this.result$.pipe(
-    map((date) => date.toLocaleDateString())
+    }, new Date(Date.UTC(2023, 4, 6)))
   );
 
   constructor() {}
-
-  public setDate(event: Event) {
-    const date = (event.target as HTMLInputElement).valueAsDate;
-    if (!date) return;
-    const utcDate = new Date(
-      Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
-    );
-    this.date$.next(utcDate ?? new Date());
-  }
-
-  public navigateNext(): void {
-    this.navigate$.next(1);
-  }
 }
